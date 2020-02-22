55f5d50188d3a80884d9978a2e3ce4ba8cf0e706
ssatyamchauhan <ssatyamchauhan1998@gmail.com>
2020-02-22 17:00:36 +0530

    Updates

---
+++ precache-manifest.6c083afef6db0f7afe9cf12eb452afce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebcbc0877f8106ecffc88e59be33d2bc",
+    "url": "/myway-videoplayer/index.html"
+  },
+  {
+    "revision": "7ccfe3f92821860a6897",
+    "url": "/myway-videoplayer/static/css/main.930e1ee9.chunk.css"
+  },
+  {
+    "revision": "9f188be8718a5dd13aed",
+    "url": "/myway-videoplayer/static/js/2.7a9a5bea.chunk.js"
+  },
+  {
+    "revision": "256b892e56b7417b1bb9cd4d8ba69983",
+    "url": "/myway-videoplayer/static/js/2.7a9a5bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ccfe3f92821860a6897",
+    "url": "/myway-videoplayer/static/js/main.062aa9bf.chunk.js"
+  },
+  {
+    "revision": "9b2dee76705d196c59bd",
+    "url": "/myway-videoplayer/static/js/runtime-main.b0185d3f.js"
+  }
+]);
